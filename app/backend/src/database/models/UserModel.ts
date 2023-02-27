d824a0c70d3814fe14f3fd5c3a6d8bfc9dd555e8
Arthur Araujo <technoasti@gmail.com>
2023-02-27 17:32:15 -0300

    iniciando o projeto TFC

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,47 @@
+import {
+  Model,
+  INTEGER,
+  STRING,
+} from 'sequelize';
+
+import db from '.';
+
+class UserModel extends Model {
+  declare readonly id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+UserModel.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: STRING,
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  underscored: true,
+  timestamps: false,
+  modelName: 'users',
+});
+
+export default UserModel;
